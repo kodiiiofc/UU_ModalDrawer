0438f0b449a94ca429c6141eb3b49b8bc08a729a
Roman Mokhnatkin <kodiiiofc@gmail.com>
2024-12-05 10:42:59 +0500

    Домашнее задание по теме "ModalDrawer"

---
+++ app/src/main/java/com/kodiiiofc/urbanuniversity/jetpackcompose/modaldrawer/MainActivity.kt
@@ -0,0 +1,273 @@
+package com.kodiiiofc.urbanuniversity.jetpackcompose.modaldrawer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.kodiiiofc.urbanuniversity.jetpackcompose.modaldrawer.ui.theme.ModalDrawerTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            val selectedNote = remember {
+                mutableStateOf<NoteItem?>(null)
+            }
+
+
+            val notesList = remember {
+                mutableStateListOf<NoteItem>()
+            }
+
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = "MainView"
+            ) {
+                composable("MainView") {
+                    MainView(navController, notesList, selectedNote)
+                }
+                composable("CreatingNoteView") {
+                    CreatingNoteView(navController, notesList)
+                }
+            }
+        }
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainView(
+    navController: NavController,
+    notesList: SnapshotStateList<NoteItem>,
+    selectedNote: MutableState<NoteItem?>
+) {
+
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalDrawerTheme {
+
+        ModalNavigationDrawer(
+            drawerContent = {
+                NavMenu(notesList, selectedNote, drawerState)
+            },
+            drawerState = drawerState,
+            content = {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = selectedNote.value?.title ?: "Заметки")
+                            },
+                            navigationIcon = {
+                                IconButton(
+                                    onClick = { scope.launch { drawerState.open() } },
+                                    content = {
+                                        Icon(Icons.Filled.Menu, "Список заметок")
+                                    }
+                                )
+                            },
+                            actions = {
+                                IconButton(
+                                    onClick = {
+                                        notesList.remove(selectedNote.value)
+                                        selectedNote.value = null
+                                    }) {
+                                    Icon(Icons.Default.Delete, "Удалить заметку")
+                                }
+                            }
+                        )
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = {
+                                navController.navigate("CreatingNoteView")
+                            }
+                        ) {
+                            Row(
+                                Modifier.padding(8.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Icon(Icons.Default.Add, "Добавить заметку")
+                                Text("Новая заметка")
+                            }
+                        }
+
+                    },
+
+                    ) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .padding(8.dp)
+                    ) {
+                        Text(
+                            text = selectedNote.value?.content ?: ""
+                        )
+                    }
+                }
+            })
+
+
+    }
+}
+
+@Composable
+fun NavMenu(
+    noteList: SnapshotStateList<NoteItem>,
+    selectedItem: MutableState<NoteItem?>,
+    drawerState: DrawerState
+) {
+    val scope = rememberCoroutineScope()
+
+    ModalDrawerSheet {
+        noteList.forEachIndexed { index, note ->
+            NavigationDrawerItem(
+                label = {
+                    Text(note.title)
+                },
+                selected = note == selectedItem.value,
+                onClick = {
+                    selectedItem.value = noteList[index]
+                    scope.launch { drawerState.close() }
+                }
+            )
+        }
+    }
+}
+
+data class NoteItem(
+    val title: String,
+    val content: String
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CreatingNoteView(navController: NavController, noteList: SnapshotStateList<NoteItem>) {
+    ModalDrawerTheme {
+
+        val snackbarHostState = remember { SnackbarHostState() }
+        val scope = rememberCoroutineScope()
+
+        var noteTitle by remember {
+            mutableStateOf("")
+        }
+
+        var noteContent by remember {
+            mutableStateOf("")
+        }
+
+        val noteHasContent by
+        remember {
+            derivedStateOf {
+                noteTitle.isNotBlank() && noteContent.isNotBlank()
+            }
+        }
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Создание заметки") },
+                    navigationIcon = {
+                        IconButton(onClick = { navController.navigateUp() }) {
+                            Icon(Icons.Default.ArrowBack, "Назад")
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = {
+                            if (noteHasContent) {
+                                noteList.add(NoteItem(noteTitle, noteContent))
+                                navController.popBackStack()
+                            } else scope.launch {
+                                snackbarHostState.showSnackbar("Поля заметки не должны быть пустыми")
+                            }
+                        }) {
+                            Icon(Icons.Default.Add, "Создать заметку")
+                        }
+                    }
+                )
+            },
+            snackbarHost = { SnackbarHost(snackbarHostState) }
+        )
+        { innerPadding ->
+            Column(
+                Modifier
+                    .padding(innerPadding)
+                    .padding(8.dp)
+            ) {
+                TextField(
+                    value = noteTitle,
+                    onValueChange = { noteTitle = it },
+                    modifier = Modifier.fillMaxWidth(),
+                    textStyle = TextStyle(
+                        fontWeight = FontWeight.Bold
+                    ),
+                    singleLine = true,
+                    placeholder = { Text("Заголовок") }
+                )
+
+                TextField(
+                    value = noteContent,
+                    onValueChange = { noteContent = it },
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth(),
+                    singleLine = false,
+                    placeholder = { Text("Текст заметки") }
+                )
+
+            }
+        }
+    }
+
+}
